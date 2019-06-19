e4804d21112e37d137236b707a7f145298879f4e
Rohit <rohitpratti@gmail.com>
2019-06-19 08:30:56 -0700

    fixed api communicator

--- lib/api-communicator.rb
+++ lib/api-communicator.rb
@@ -5,5 +5,5 @@ require 'net/http'
 
 word = small #we need to tie this to the word pool
 
-x = RestClient.get(https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/+word?strictMatch=false, headers={app_id ="ce9c707e" app_key = "bb1b3f7c5a8edc93aba9be741f039537"})
+x = RestClient.get('https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/+word?strictMatch=false, headers={app_id ="ce9c707e" app_key = "bb1b3f7c5a8edc93aba9be741f039537" }')
 y = JSON.parse(x)
